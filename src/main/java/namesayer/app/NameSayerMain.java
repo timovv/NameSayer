041447e4380979750e895310c81339992ea75209
Timo van Veenendaal <me@timo.nz>
2018-08-31 23:32:26 +1200

    Add project hierarchy
    Fixes #1

---
+++ src/main/java/namesayer/app/NameSayerMain.java
@@ -0,0 +1,7 @@
+package namesayer.app;
+
+public class NameSayerMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/namesayer/app/NameSayerTest.java
@@ -0,0 +1,18 @@
+package namesayer.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * not an actual test
+ */
+public class NameSayerTest {
+    /**
+     * i love unit testing
+     */
+    @Test
+    public void testNothing() {
+        assertTrue(true);
+    }
+}
