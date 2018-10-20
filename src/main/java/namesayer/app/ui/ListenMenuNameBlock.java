cc1ed78569e338f4734d476fbc597613edd10026
Timo van Veenendaal <me@timo.nz>
2018-10-20 13:33:16 +1300

    Remove expando from Name block in listen menu
    Doesn't make sense having this anymore, since attempts are now displayed
    in the attempts tab.

--- src/main/java/namesayer/app/ui/ListenMenuNameBlock.java
+++ src/main/java/namesayer/app/ui/ListenMenuNameBlock.java
@@ -23,32 +23,9 @@ public class ListenMenuNameBlock extends NameBlock {
     private Text dateText;
 
     @FXML
-    private StackPane expandoExpand;
-
-    @FXML
-    private StackPane expandoUnexpand;
-
-    @FXML
     private VBox attemptsBox;
-
-    private BooleanProperty expanded;
 
     public ListenMenuNameBlock(NameSayerDatabase db, NameSayerShop shop, Name name) {
         super(name, db, shop, ListenMenuNameBlock.class.getResource("/fxml/nameBlockMain.fxml"));
-        expanded = new SimpleBooleanProperty(false);
-
-        // show the correct (expand vs unexpand) button based on whether attempts are visible or not
-        expandoExpand.visibleProperty().bind(expanded.not());
-        expandoUnexpand.visibleProperty().bind(expanded);
-    }
-
-    @FXML
-    private void onExpandClicked() {
-        expanded.set(true);
-    }
-
-    @FXML
-    private void onUnexpandClicked() {
-        expanded.set(false);
     }
 }
